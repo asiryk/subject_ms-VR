80878cf025efd910a3d1a61e503aac9f4c0eabb9
asiryk <asiryk.net@gmail.com>
2022-11-03 03:01:31 +0200

    feat: add glsl page

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://asiryk.github.io',
-  base: '/subject_ms-VGGL',
-});
---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import glsl from "vite-plugin-glsl";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://asiryk.github.io",
+  base: "/subject_ms-VGGL",
+  vite: {
+    plugins: [glsl()],
+  },
+});
