fd8ff9a33a0e333313752945639337c6a7208e4c
asiryk <asiryk.net@gmail.com>
2022-11-04 15:46:22 +0200

    chore: rename static path

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import glsl from "vite-plugin-glsl";
 // https://astro.build/config
 export default defineConfig({
   site: "https://asiryk.github.io",
-  base: "/subject_ms-VGGL",
+  base: "/subject_ms-VGGI",
   vite: {
     plugins: [glsl()],
   },
