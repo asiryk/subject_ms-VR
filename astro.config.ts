a4fafe2c3e7ed35c8fda080dc807eba155254b6b
asiryk <asiryk.net@gmail.com>
2022-11-04 20:51:58 +0200

    feat: set up code style and tooling
    install eslint, prettier

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config";
 import glsl from "vite-plugin-glsl";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://asiryk.github.io",
   base: "/subject_ms-VGGI",
   vite: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     plugins: [glsl()],
   },
+  integrations: [tailwind()],
 });
